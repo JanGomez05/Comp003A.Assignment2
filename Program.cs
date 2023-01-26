b09417eff7b03522a1f1ede44e58c080eb895cd9
Jan Gomez <123139695+JanGomez05@users.noreply.github.com>
2023-01-26 12:36:54 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Author: Jan Gomez
+ * Course: Comp 003A
+ * Purpose: To finish assignment 2
+ */
+ namespace Comp003A.Assignment2_;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = "COMP-003A - Assignment 2";
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.ForegroundColor = ConsoleColor.White;
+        
+        string firstName = Console.ReadLine();
+        firstName = "Enter Your Name:";
+        
+        string middleName = Console.ReadLine();
+        middleName = "Enter Your Middle Name:";
+
+        
+        string lastName = Console.ReadLine();
+        lastName = "Enter Your Last Name:";
+
+        string inputAge = Console.ReadLine();
+
+
+        int yearBorn = 2023 - Convert.ToInt32(inputAge);
+
+        Console.WriteLine($"Hello, {firstName} {middleName} {lastName}. You were born in {yearBorn}.");
+
+
+
+        Console.WriteLine("Hello, World!");
+    }
+}
+
